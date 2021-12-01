0e245118c87665006b8b5915614feb1c54c77253
ujjwallamsal <ujjwallamsal96@gmail.com>
2021-12-01 18:27:23 +0545

    this is password authenticatio

---
+++ password.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"ujjwal.lamsal" : "876363" , "python.class" : "656521"}
+username = input("enter your username : ")
+password = input("enter your passwword : ")
+for ele in database.keys():
+    if username == ele :
+        while password != database.get(ele):
+            password = getpass.getpass("enter your password")
+        break
+print("verified")
